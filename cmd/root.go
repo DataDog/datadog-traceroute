db84d80335ffbf0d901b5a035c742cf38f5a88ac
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-07-29 13:57:18 +0200

    fix import

--- cmd/root.go
+++ cmd/root.go
@@ -17,19 +17,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/DataDog/datadog-traceroute/common"
-	"github.com/DataDog/datadog-traceroute/icmp"
-	"github.com/DataDog/datadog-traceroute/log"
-	"github.com/DataDog/datadog-traceroute/pinger"
-	"github.com/DataDog/datadog-traceroute/sack"
-	"github.com/DataDog/datadog-traceroute/tcp"
-	"github.com/DataDog/datadog-traceroute/udp"
-
 	"github.com/spf13/cobra"
 
 	"github.com/DataDog/datadog-traceroute/common"
 	"github.com/DataDog/datadog-traceroute/icmp"
 	"github.com/DataDog/datadog-traceroute/log"
+	"github.com/DataDog/datadog-traceroute/pinger"
 	"github.com/DataDog/datadog-traceroute/sack"
 	"github.com/DataDog/datadog-traceroute/tcp"
 	"github.com/DataDog/datadog-traceroute/udp"
