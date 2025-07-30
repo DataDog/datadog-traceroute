3a0a4d4faa68e9871d7ab0f214025e08dcac9a92
Fabrice Matrat <fabrice.matrat@datadoghq.com>
2025-07-30 11:08:50 +0200

    Adapt log to be replaced in datadog-agent.

--- log/log.go
+++ log/log.go
@@ -13,44 +13,114 @@ func SetVerbose(v bool) {
 	enabled = v
 }
 
+type Logger struct {
+	Tracef    func(format string, args ...interface{})
+	Trace     func(format string)
+	Infof     func(format string, args ...interface{})
+	Debugf    func(format string, args ...interface{})
+	Warnf     func(format string, args ...interface{}) error
+	Errorf    func(format string, args ...interface{}) error
+	TraceFunc func(func() string)
+}
+
+var logger = Logger{
+	Tracef:    defaultTracef,
+	Trace:     defaultTrace,
+	Infof:     defaultInfof,
+	Debugf:    defaultDebugf,
+	Warnf:     defaultWarnf,
+	Errorf:    defaultErrorf,
+	TraceFunc: defaultTraceFunc,
+}
+
+func SetLogger(l Logger) {
+	logger = l
+}
+
+func Tracef(format string, args ...interface{}) {
+	if logger.Tracef != nil {
+		logger.Tracef(format, args...)
+	}
+}
+
+func Trace(format string, args ...interface{}) {
+	if logger.Trace != nil {
+		logger.Trace(format)
+	}
+}
+
+func Infof(format string, args ...interface{}) {
+	if logger.Infof != nil {
+		logger.Infof(format, args...)
+	}
+}
+
+func Debugf(format string, args ...interface{}) {
+	if logger.Debugf != nil {
+		logger.Debugf(format, args...)
+	}
+}
+
+func Warnf(format string, args ...interface{}) error {
+	if logger.Warnf != nil {
+		return logger.Warnf(format, args...)
+	}
+	return nil
+}
+
+func Errorf(format string, args ...interface{}) error {
+	if logger.Errorf != nil {
+		return logger.Errorf(format, args...)
+	}
+	return nil
+}
+
+func TraceFunc(logFunc func() string) {
+	if logger.TraceFunc != nil {
+		logger.TraceFunc(logFunc)
+	}
+}
+
 var (
-	Tracef = func(format string, args ...interface{}) {
+	defaultTracef = func(format string, args ...interface{}) {
 		if enabled {
 			log.Printf("[TRACE] "+format, args...)
 		}
 	}
 
-	Trace = func(format string) {
+	defaultTrace = func(format string) {
 		if enabled {
 			log.Print("[TRACE] " + format)
 		}
 	}
 
-	Infof = func(format string, args ...interface{}) {
+	defaultInfof = func(format string, args ...interface{}) {
 		if enabled {
 			log.Printf("[INFO] "+format, args...)
 		}
 	}
 
-	Debugf = func(format string, args ...interface{}) {
+	defaultDebugf = func(format string, args ...interface{}) {
 		if enabled {
 			log.Printf("[DEBUG] "+format, args...)
 		}
 	}
 
-	Errorf = func(format string, args ...interface{}) {
+	defaultErrorf = func(format string, args ...interface{}) error {
 		if enabled {
 			log.Printf("[ERROR] "+format, args...)
 		}
+		return nil
 	}
 
-	Warnf = func(format string, args ...interface{}) {
+	defaultWarnf = func(format string, args ...interface{}) error {
 		if enabled {
 			log.Printf("[WARN] "+format, args...)
 		}
+		return nil
 	}
 
-	TraceFunc = func(logFunc func() string) {
+	defaultTraceFunc = func(logFunc func() string) {
 		if enabled {
 			log.Print("[TRACEFUNC] " + logFunc())
 		}
