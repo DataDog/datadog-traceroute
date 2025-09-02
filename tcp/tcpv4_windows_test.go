ddf7e36ec1dc6fb1a7734768d5c17e54e963b92a
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-09-02 11:37:44 +0200

    remove unused

--- tcp/tcpv4_windows_test.go
+++ tcp/tcpv4_windows_test.go
@@ -20,7 +20,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/DataDog/datadog-traceroute/common"
 	"github.com/DataDog/datadog-traceroute/result"
 	"github.com/DataDog/datadog-traceroute/winconn"
 )
