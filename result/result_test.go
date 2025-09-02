42f182bbc9fa7e5144875a02a6d70f27f669c26d
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-09-02 11:19:16 +0200

    add test case

--- result/result_test.go
+++ result/result_test.go
@@ -71,6 +71,61 @@ func TestResults_Normalize(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "destination not reachable",
+			Results: Results{
+				Traceroute: Traceroute{
+					Runs: []TracerouteRun{
+						{
+							Hops: []*TracerouteHop{
+								{IP: "10.10.10.10", RTT: 10},
+								{},
+								{IP: "10.10.10.10", RTT: 30},
+								{IP: "10.10.10.10", RTT: 30},
+							},
+						},
+						{
+							Hops: []*TracerouteHop{
+								{IP: "10.10.10.10", RTT: 10},
+								{IP: "10.10.10.10", RTT: 20},
+							},
+						},
+					},
+				},
+			},
+			ExpectedResults: Results{
+				Traceroute: Traceroute{
+					Runs: []TracerouteRun{
+						{
+							Hops: []*TracerouteHop{
+								{IP: "10.10.10.10", RTT: 10},
+								{},
+								{IP: "10.10.10.10", RTT: 30},
+								{IP: "10.10.10.10", RTT: 30},
+							},
+						},
+						{
+							Hops: []*TracerouteHop{
+								{IP: "10.10.10.10", RTT: 10},
+								{IP: "10.10.10.10", RTT: 20},
+							},
+						},
+					},
+					HopCount: HopCountStats{
+						Avg: 3,
+						Min: 2,
+						Max: 4,
+					},
+				},
+				E2eProbe: E2eProbe{
+					Rtts:                 []float64{},
+					PacketsSent:          1,
+					PacketsReceived:      0,
+					PacketLossPercentage: 1,
+					Rtt:                  E2eProbeRttLatency{},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
