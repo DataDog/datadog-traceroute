c3b7ab86605e96f5200c602cb7e4ba0d9774d3d1
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-10-29 17:00:20 +0100

    More reliable e2e probe packet loss and rtt (#58)
    Co-authored-by: alexandre.yang <alexandre.yang@datadoghq.com>

--- runner/runner.go
+++ runner/runner.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/DataDog/datadog-traceroute/common"
 	"github.com/DataDog/datadog-traceroute/icmp"
+	"github.com/DataDog/datadog-traceroute/log"
 	"github.com/DataDog/datadog-traceroute/result"
 	"github.com/DataDog/datadog-traceroute/sack"
 	"github.com/DataDog/datadog-traceroute/tcp"
@@ -73,23 +74,33 @@ func runTracerouteMulti(ctx context.Context, params TracerouteParams, destinatio
 		}()
 	}
 
-	// e2e probes
-	for i := 0; i < params.E2eQueries; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			e2eRtt, err := runE2eProbeOnce(ctx, params, destinationPort)
-			resultsAndErrorsMu.Lock()
-			if err != nil {
-				multiErr = append(multiErr, err)
-				results.E2eProbe.RTTs = append(results.E2eProbe.RTTs, 0.0)
-			} else {
-				results.E2eProbe.RTTs = append(results.E2eProbe.RTTs, e2eRtt)
+	if params.E2eQueries > 0 {
+		// e2eQueriesDelay is currently calculated based on "MaxTTL * Timeout / e2e queries"
+		// but should be replaced by "Timeout / e2e queries" once we change the meaning of Timeout param to be global vs per call.
+		// Related Jira ticket: CNM-4763 datadog-traceroute library should provide global timeout option instead of per call
+		e2eQueriesDelay := (time.Duration(params.MaxTTL) * params.Timeout) / time.Duration(params.E2eQueries)
+		log.Tracef("e2e query delay: %d msec", e2eQueriesDelay.Milliseconds())
+		// e2e probes
+		for i := 0; i < params.E2eQueries; i++ {
+			log.Tracef("send e2e probe #%d", i+1)
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				e2eRtt, err := runE2eProbeOnce(ctx, params, destinationPort)
+				resultsAndErrorsMu.Lock()
+				if err != nil {
+					multiErr = append(multiErr, err)
+					results.E2eProbe.RTTs = append(results.E2eProbe.RTTs, 0.0)
+				} else {
+					results.E2eProbe.RTTs = append(results.E2eProbe.RTTs, e2eRtt)
+				}
+				resultsAndErrorsMu.Unlock()
+			}()
+			if i < (params.E2eQueries - 1) { // don't add delay for last query
+				time.Sleep(e2eQueriesDelay)
 			}
-			resultsAndErrorsMu.Unlock()
-		}()
+		}
 	}
-
 	wg.Wait()
 	if len(multiErr) > 0 {
 		return nil, errors.Join(multiErr...)
