e1c437e6706df3e8ffb07c78b58cc0654a2c0036
Jim Wilson <86026167+jmw51798@users.noreply.github.com>
2025-12-12 10:04:05 -0700

    more

--- common/utils.go
+++ common/utils.go
@@ -7,9 +7,16 @@ import (
 )
 
 func ConvertDurationToMs(duration time.Duration) float64 {
+	// Test Windows time.Now() precision by calling it twice in a row
+	t1 := time.Now()
+	t2 := time.Now()
+	timeDiff := t2.Sub(t1)
+	log.Debugf("ConvertDurationToMs: time.Now() precision test: t1=%v t2=%v diff=%v (%d ns)",
+		t1, t2, timeDiff, timeDiff.Nanoseconds())
+
 	seconds := duration.Seconds()
 	milliseconds := seconds * 1000
-	log.Debugf("ConvertDurationToMs: duration=%v (nanoseconds=%d) seconds=%f milliseconds=%f", 
+	log.Debugf("ConvertDurationToMs: duration=%v (nanoseconds=%d) seconds=%f milliseconds=%f",
 		duration, duration.Nanoseconds(), seconds, milliseconds)
 	return milliseconds
 }
