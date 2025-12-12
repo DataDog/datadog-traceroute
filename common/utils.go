aadc964e1d2d0f0e823bc57d6621cd64b095879a
Jim Wilson <86026167+jmw51798@users.noreply.github.com>
2025-12-12 10:12:49 -0700

    more

--- common/utils.go
+++ common/utils.go
@@ -1,18 +1,33 @@
 package common
 
 import (
+	"sync"
 	"time"
 
 	"github.com/DataDog/datadog-traceroute/log"
 )
 
+var (
+	timePrecisionTestOnce sync.Once
+)
+
 func ConvertDurationToMs(duration time.Duration) float64 {
-	// Test Windows time.Now() precision by calling it twice in a row
-	t1 := time.Now()
-	t2 := time.Now()
-	timeDiff := t2.Sub(t1)
-	log.Debugf("ConvertDurationToMs: time.Now() precision test: t1=%v t2=%v diff=%v (%d ns)",
-		t1, t2, timeDiff, timeDiff.Nanoseconds())
+	// Test Windows time.Now() precision (only on first call)
+	timePrecisionTestOnce.Do(func() {
+		t1 := time.Now()
+		t2 := t1
+		iterations := 0
+
+		// Keep calling time.Now() until we get a different value
+		for t2.Equal(t1) && iterations < 1000000 {
+			t2 = time.Now()
+			iterations++
+		}
+
+		timeDiff := t2.Sub(t1)
+		log.Debugf("ConvertDurationToMs: time.Now() precision test: t1=%v t2=%v diff=%v (%d ns) iterations=%d",
+			t1, t2, timeDiff, timeDiff.Nanoseconds(), iterations)
+	})
 
 	seconds := duration.Seconds()
 	milliseconds := seconds * 1000
