34974d851efb681c5077b2bdf351d12b86f73115
Alexandre Yang <alexandre.yang@datadoghq.com>
2026-02-17 20:34:12 +0100

    Apply suggestion from @AlexandreYang

--- result/utils.go
+++ result/utils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/uuid"
 )
 
+// encode UUID with base64 for shorter UUID
 func newBase64UUID() string {
 	id := uuid.New()
 	return base64.RawURLEncoding.EncodeToString(id[:])
